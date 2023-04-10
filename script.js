e2225f3d29331327c5eb4a3ef27b2cf6327dac8a
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-04-10 15:46:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+var table = document.querySelector('table');
+ var lastRow = document.createElement('tr');
+ var prices = document.querySelectorAll('[data-ns-test=price]');
+ let sum = 0;
+ for(let i = 0; i < prices.length; i++) {
+  sum += parseInt(prices[i].textContent);
+ }
+ var child = document.createElement("td");
+ child.setAttribute('data-ns-test', 'grandTotal');
+ 
+
+ child.textContent = sum;
+ lastRow.appendChild(child);
+ table.appendChild(lastRow);
